46061d4f68ab5f753a5086a32acec5c67cd02463
Clément Sicard <clement.sicard@epfl.ch>
2020-11-24 18:17:31 +0100

    added handling CTRL-C graceful interrupt

--- automove.py
+++ automove.py
@@ -4,38 +4,45 @@ import glob
 import time
 import configparser
 
-PATH_TO_INI_FILE = "ABSOLUTE PATH TO YOUR CONFIG.INI FILE"
 
-config = configparser.ConfigParser()
-config.read(PATH_TO_INI_FILE)
+def main():
+    PATH_TO_INI_FILE = "/Users/clementsicard/Dev/AutoMove/config.ini"
+
+    config = configparser.ConfigParser()
+    config.read(PATH_TO_INI_FILE)
+
+    onedrive_path = config["PATHS"]["onedrive_path"]
+    icloud_path = config["PATHS"]["icloud_path"]
+
+    nothing = False
+
+    while True:
+        icloud_folders = [folder for folder in os.listdir(
+            icloud_path) if folder[0] != "."]
+
+        files = []
+        for folder in icloud_folders:
+            relative_paths_files = [path[len(onedrive_path) - 1:] for path in
+                                    glob.glob(onedrive_path + folder + "/*.*", recursive=True)]
+            if relative_paths_files != []:
+                files.extend(relative_paths_files)
+
+        if files != []:
+            nothing = False
+            print("[AutoMove] Moving", len(files),
+                  "file(s) from OneDrive to iCloud...")
+            for f in files:
+                shutil.move(onedrive_path[:-1] + f, icloud_path +
+                            f, copy_function=shutil.copy)
+
+            print("[AutoMove] Success!")
+        elif nothing == False:
+            nothing = True
+            print("[AutoMove] Nothing to do for now.\n")
 
 
-onedrive_path = config["PATHS"]["onedrive_path"]
-icloud_path = config["PATHS"]["icloud_path"]
-
-nothing = False
-
-while True:
-    icloud_folders = [folder for folder in os.listdir(
-        icloud_path) if folder[0] != "."]
-
-    files = []
-    for folder in icloud_folders:
-        relative_paths_files = [path[58:] for path in
-                                glob.glob(onedrive_path + folder + "/*.*", recursive=True)]
-        if relative_paths_files != []:
-            files.extend(relative_paths_files)
-
-    if files != []:
-        nothing = False
-        print("[AutoMove] Moving", len(files),
-              "file(s) from OneDrive to iCloud...")
-        for f in files:
-            shutil.move(onedrive_path[:-1] + f, icloud_path +
-                        f, copy_function=shutil.copy)
-
-        print("[AutoMove] Success!")
-    elif nothing == False:
-        nothing = True
-        print("[AutoMove] Nothing to do for now.\n")
-    time.sleep(2)
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
